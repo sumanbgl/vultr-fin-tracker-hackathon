b5a3e7b5ac45c9ea743aa1465eb608324692eaf1
Suman P S <suman.bgl@gmail.com>
2024-11-11 23:54:08 +0530

    Backend initial version.

---
+++ springboot-be/vultr-fintracker/src/main/java/com/vultr/vultrfintracker/VultrFintrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vultr.vultrfintracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VultrFintrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VultrFintrackerApplication.class, args);
+    }
+
+}
---
+++ springboot-be/vultr-fintracker/src/test/java/com/vultr/vultrfintracker/VultrFintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vultr.vultrfintracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VultrFintrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
